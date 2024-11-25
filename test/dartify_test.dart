751976f5d5a3cba4a6a5c3119a441da78d373d70
rydmike <mrydstrom+gh@gmail.com>
2024-11-25 23:39:54 +0200

    ADD: Print value, type, is int, is double for JS and WASM build

--- test/dartify_test.dart
+++ test/dartify_test.dart
@@ -3,6 +3,12 @@ import 'dart:js_interop';
 import 'package:test/test.dart';
 
 void main() {
+  int i = 1;
+  print(i.toJS.dartify()); // WASM 1.0, JS 1
+  print(i.toJS.dartify().runtimeType); // WASM double, JS int
+  print(i.toJS.dartify() is int); // WASM false, JS true
+  print(i.toJS.dartify() is double); // WASM true, JS true
+
   group('dartify tests with no casts', () {
     // Int value tests
     test('dartify JSAny? value 1 and test if int?', () {
