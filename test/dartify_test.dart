a74cd211d0845f759c7fcab3fb360c6c60382516
rydmike <mrydstrom+gh@gmail.com>
2024-11-26 10:38:54 +0200

    Update the dartify test to not trigger lint: invalid_runtime_check_with_js_interop_types

--- test/dartify_test.dart
+++ test/dartify_test.dart
@@ -1,9 +1,12 @@
+// Ignored rules for this test
+// ignore_for_file: avoid_print, avoid_init_to_null, unnecessary_nullable_for_final_variable_declarations, lines_longer_than_80_chars, prefer_const_declarations
+
 import 'dart:js_interop';
 
 import 'package:test/test.dart';
 
 void main() {
-  int i = 1;
+  const int i = 1;
   print(i.toJS.dartify()); // WASM 1.0, JS 1
   print(i.toJS.dartify().runtimeType); // WASM double, JS int
   print(i.toJS.dartify() is int); // WASM false, JS true
@@ -12,46 +15,46 @@ void main() {
   group('dartify tests with no casts', () {
     // Int value tests
     test('dartify JSAny? value 1 and test if int?', () {
-      JSAny? jsValue = 1 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int? typedValue = 1;
+      final JSAny? jsValue = 1.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const int? typedValue = 1;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
     test('dartify JSAny? value 1 and test if int', () {
-      JSAny? jsValue = 1 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int typedValue = 1;
+      final JSAny? jsValue = 1.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const int typedValue = 1;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
     test('dartify JSAny? value null and test if int? null', () {
-      JSAny? jsValue = null as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int? typedValue = null;
+      final JSAny? jsValue = null as JSAny?;
+      final Object? dartValue = jsValue.dartify();
+      const int? typedValue = null;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
 
     // Double value tests
     test('dartify JSAny? value 1.0 and test if double?', () {
-      JSAny? jsValue = 1.0 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double? typedValue = 1;
+      final JSAny? jsValue = 1.0.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const double? typedValue = 1;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
     test('dartify JSAny? value 1.0 and test if double', () {
-      JSAny? jsValue = 1.0 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double typedValue = 1.0;
+      final JSAny? jsValue = 1.0.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const double typedValue = 1.0;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
     test('dartify JSAny? value null and test if double? null', () {
-      JSAny? jsValue = null as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double? typedValue = null;
+      final JSAny? jsValue = null as JSAny?;
+      final Object? dartValue = jsValue.dartify();
+      const double? typedValue = null;
       print('dartValue: $dartValue');
       expect(dartValue, equals(typedValue));
     });
@@ -60,46 +63,46 @@ void main() {
   group('dartify tests with casts', () {
     // Int value tests
     test('dartify JSAny? value 1 and test if int?', () {
-      JSAny? jsValue = 1 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int? typedValue = 1;
+      final JSAny? jsValue = 1.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const int? typedValue = 1;
       print('dartValue: $dartValue');
       expect(dartValue as int?, equals(typedValue));
     });
     test('dartify JSAny? value 1 and test if int', () {
-      JSAny? jsValue = 1 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int typedValue = 1;
+      final JSAny? jsValue = 1.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const int typedValue = 1;
       print('dartValue: $dartValue');
-      expect(dartValue as int, equals(typedValue));
+      expect(dartValue as int?, equals(typedValue));
     });
     test('dartify JSAny? value null and test if int? null', () {
-      JSAny? jsValue = null as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      int? typedValue = null;
+      final JSAny? jsValue = null as JSAny?;
+      final Object? dartValue = jsValue.dartify();
+      const int? typedValue = null;
       print('dartValue: $dartValue');
       expect(dartValue as int?, equals(typedValue));
     });
 
     // Double value tests
     test('dartify JSAny? value 1.0 and test if double?', () {
-      JSAny? jsValue = 1.0 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double? typedValue = 1;
+      final JSAny? jsValue = 1.0.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const double? typedValue = 1;
       print('dartValue: $dartValue');
       expect(dartValue as double?, equals(typedValue));
     });
     test('dartify JSAny? value 1.0 and test if double', () {
-      JSAny? jsValue = 1 as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double typedValue = 1.0;
+      final JSAny? jsValue = 1.toJS;
+      final Object? dartValue = jsValue.dartify();
+      const double typedValue = 1.0;
       print('dartValue: $dartValue');
-      expect(dartValue as double, equals(typedValue));
+      expect(dartValue as double?, equals(typedValue));
     });
     test('dartify JSAny? value null and test if double? null', () {
-      JSAny? jsValue = null as JSAny?;
-      Object? dartValue = jsValue.dartify();
-      double? typedValue = null;
+      final JSAny? jsValue = null as JSAny?;
+      final Object? dartValue = jsValue.dartify();
+      const double? typedValue = null;
       print('dartValue: $dartValue');
       expect(dartValue as double?, equals(typedValue));
     });
